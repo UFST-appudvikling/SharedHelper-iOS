310e6355f7aed1b0ca0e4d45e2e030b3dd4870dd
Emad Ghorbaninia <emadgnia@gmail.com>
2023-04-03 10:55:59 +0200

    more fix

--- Sources/Authentication/AuthenticationHandler.swift
+++ Sources/Authentication/AuthenticationHandler.swift
@@ -304,7 +304,7 @@ extension AuthenticationHandler: ASWebAuthenticationPresentationContextProviding
 
 
 extension AuthenticationHandler {
-    public static func getPayload<Payload: Decodable>(accessToken: String)-> Payload? {
+    public static func getPayload<Payload: Decodable>(_ type: Payload.Type, accessToken: String)-> Payload? {
         let encodedData = { (string: String) -> Data? in
             var encodedString = string.replacingOccurrences(of: "-", with: "+").replacingOccurrences(of: "_", with: "/")
             
