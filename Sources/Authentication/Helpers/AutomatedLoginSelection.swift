b3265fefaf4357fc6c4e98393d8c7e6ef716ab81
Nicolai Øyen Dam <nicolai.dam@ufst.dk>
2023-05-26 10:57:29 +0200

    Clean up AutomatedLoginSelection few

--- Sources/Authentication/Helpers/AutomatedLoginSelection.swift
+++ Sources/Authentication/Helpers/AutomatedLoginSelection.swift
@@ -7,6 +7,40 @@
 
 import SwiftUI
 
+/// User selection view used for automated login
+/// - Example:
+/// ````
+///struct AutomatedLoginButton: View {
+///
+///    @State var showAutomatedLogin = false
+///    let jsonFileURL: URL
+///    let filterUsers: AutomatedLoginSelection.FilterUsers
+///    let onCallback: (AutomatedLoginSelection.Callback) -> Void
+///
+///    var body: some View {
+///        Button("Automated Login") {
+///            showAutomatedLogin = true
+///        }
+///        .padding()
+///        .sheet(
+///            isPresented: $showAutomatedLogin,
+///            content: {
+///                AutomatedLoginSelection(
+///                    jsonFilePath: jsonFileURL,
+///                    filterUsers: filterUsers
+///                ) {
+///                    onCallback($0)
+///                    self.showAutomatedLogin = false
+///                }
+///            }
+///        )
+///    }
+///}
+/// ````
+/// - Returns: View
+/// - Parameter jsonFilePath: File path for json file, could for example be: Bundle.main.url(forResource: "automated_login_ejerskifte", withExtension: "json")!
+/// - Parameter filterUsers: Enum of type FilterUsers used to filter user (relevant if there is both azure and dcs users in the json file)
+/// - Parameter callback: Is triggered when there is picked a user
 public struct AutomatedLoginSelection: View {
     
     @State var users: [UserItem] = []
@@ -30,112 +64,127 @@ public struct AutomatedLoginSelection: View {
     
     public var body: some View {
         NavigationView {
-            Group {
-                if let error {
-                    Text(error)
-                } else {
-                    List {
-                        Section("Manual") {
-                            Button {
-                                callback(.Manual)
-                            } label: {
-                                Text("Choose OAuth login")
-                                    .foregroundColor(Color.cyan)
-                                    .font(.title2)
-                            }
-                        }
-                        Section("Automated") {
-                            Picker(
-                                "Select user",
-                                selection: $selectedUser
-                            ) {
-                                ForEach(users, id: \.self) { user in
-                                    switch user {
-                                    case .azure(let azureUser):
-                                        Text(azureUser.title)
-                                            .tag(azureUser.title)
-                                    case .dcs(let dcsUser):
-                                        Text(dcsUser.title)
-                                            .tag(dcsUser.title)
-                                    }
-                                }
-                            }
-                            Button {
-                                for user in users {
-                                    switch user {
-                                    case .dcs(let automatedLoginDCSUser):
-                                        if automatedLoginDCSUser.title == selectedUser {
-                                            callback(.Automated(.dcs(automatedLoginDCSUser), self.url))
-                                        }
-                                    case .azure(let automatedLoginAzureUser):
-                                        if automatedLoginAzureUser.title == selectedUser {
-                                            callback(.Automated(.azure(automatedLoginAzureUser), self.url))
-                                        }
-                                    }
-                                }
-                            } label: {
-                                Text("Choose automated login")
-                                    .foregroundColor(Color.cyan)
-                                    .font(.title2)
-                            }
-                        }
+            content
+            .navigationTitle("QA Login")
+            .onAppear { onAppear() }
+        }
+    }
+}
+
+private extension AutomatedLoginSelection {
+    var content: some View {
+        Group {
+            if let error {
+                Text(error)
+            } else {
+                List {
+                    userPicker
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var userPicker: some View {
+        Section("Manual") {
+            Button {
+                callback(.Manual)
+            } label: {
+                Text("Choose OAuth login")
+                    .foregroundColor(Color.cyan)
+                    .font(.title2)
+            }
+        }
+        Section("Automated") {
+            Picker(
+                "Select user",
+                selection: $selectedUser
+            ) {
+                ForEach(users, id: \.self) { user in
+                    switch user {
+                    case .azure(let azureUser):
+                        Text(azureUser.title)
+                            .tag(azureUser.title)
+                    case .dcs(let dcsUser):
+                        Text(dcsUser.title)
+                            .tag(dcsUser.title)
                     }
                 }
             }
-            .navigationTitle("QA Login")
-            .onAppear {
-                do {
-                    
-                    let data = try Data(contentsOf: self.jsonFilePath)
-                    let decoder = JSONDecoder()
-                    let jsonData = try decoder.decode(AuthenticationHandler.AutomatedLoginJSON.self, from: data)
-                    
-                    switch self.filterUsers {
-                    case .all:
-                        self.users = jsonData.users
-                    case .dcsOnly:
-                        for user in jsonData.users {
-                            if case .dcs = user {
-                                self.users.append(user)
-                            }
-                        }
-                    case .azureOnly:
-                        for user in jsonData.users {
-                            if case .azure = user {
-                                self.users.append(user)
-                            }
-                        }
-                    }
-                    
-                    self.url = jsonData.url
-                
-                    guard let firstUser = self.users.first else { fatalError("Could not find first user") }
-                    
-                    switch firstUser {
+            Button {
+                for user in users {
+                    switch user {
                     case .dcs(let automatedLoginDCSUser):
-                        self.selectedUser = automatedLoginDCSUser.title
-                        break
+                        if automatedLoginDCSUser.title == selectedUser {
+                            callback(.Automated(.dcs(automatedLoginDCSUser), self.url))
+                        }
                     case .azure(let automatedLoginAzureUser):
-                        self.selectedUser = automatedLoginAzureUser.title
-                        break
+                        if automatedLoginAzureUser.title == selectedUser {
+                            callback(.Automated(.azure(automatedLoginAzureUser), self.url))
+                        }
                     }
-                    
-                } catch let error {
-                    self.error = "Error decoding json file: \(error.localizedDescription)"
                 }
+            } label: {
+                Text("Choose automated login")
+                    .foregroundColor(Color.cyan)
+                    .font(.title2)
             }
         }
     }
 }
 
-extension AutomatedLoginSelection {
+private extension AutomatedLoginSelection {
+    func onAppear() {
+        do {
+            
+            let data = try Data(contentsOf: self.jsonFilePath)
+            let decoder = JSONDecoder()
+            let jsonData = try decoder.decode(AuthenticationHandler.AutomatedLoginJSON.self, from: data)
+            
+            switch self.filterUsers {
+            case .all:
+                self.users = jsonData.users
+            case .dcsOnly:
+                for user in jsonData.users {
+                    if case .dcs = user {
+                        self.users.append(user)
+                    }
+                }
+            case .azureOnly:
+                for user in jsonData.users {
+                    if case .azure = user {
+                        self.users.append(user)
+                    }
+                }
+            }
+            
+            self.url = jsonData.url
+        
+            guard let firstUser = self.users.first else { fatalError("Could not find first user") }
+            
+            switch firstUser {
+            case .dcs(let automatedLoginDCSUser):
+                self.selectedUser = automatedLoginDCSUser.title
+                break
+            case .azure(let automatedLoginAzureUser):
+                self.selectedUser = automatedLoginAzureUser.title
+                break
+            }
+            
+        } catch let error {
+            self.error = "Error decoding json file: \(error.localizedDescription)"
+        }
+    }
+}
+
+public extension AutomatedLoginSelection {
     
-    public enum Callback: Equatable {
+    enum Callback: Equatable {
         case Manual
         case Automated(UserItem,String)
     }
     
-    public enum FilterUsers {
+    enum FilterUsers {
         case all
         case dcsOnly
         case azureOnly
