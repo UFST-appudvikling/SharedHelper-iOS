3ac22a5f5f519bdfda83568023e651977f205dab
Nicolai Øyen Dam <nicolai.dam@ufst.dk>
2023-06-15 11:28:51 +0200

    Add cleanup

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Networking",
-            dependencies: [.product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk")]),
+            dependencies: []),
         .testTarget(
             name: "NetworkingTests",
             dependencies: ["Networking"]),
